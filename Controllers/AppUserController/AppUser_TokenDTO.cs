aa55a6c53768d2200710aa54e65f8f7c674df58d
lam <105333043+NHTLam@users.noreply.github.com>
2024-04-07 19:15:30 +0700

    Sửa lại các gửi token để dễ dàng cho phép nhập hơn

---
+++ Controllers/AppUserController/AppUser_TokenDTO.cs
@@ -0,0 +1,18 @@
+﻿using CodeBE_LEM.Controllers.AppUserController;
+using CodeBE_LEM.Controllers.PermissionController;
+using CodeBE_LEM.Entities;
+using CodeBE_LEM.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CodeBE_LEM.Controllers.AppUserController;
+
+public partial class AppUser_TokenDTO
+{
+    public string? Token { get; set; }
+    public AppUser_TokenDTO() { }
+    public AppUser_TokenDTO(string token)
+    {
+        this.Token = token;
+    }
+}
